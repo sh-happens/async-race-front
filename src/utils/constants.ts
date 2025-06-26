94761a2ec36bb155c6a034e01d090434fce8715e
Yerbol <yerbol_kosbayev@list.ru>
2025-06-26 17:18:04 +0500

    feat(utils): add basic constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+export const API_BASE_URL = 'http://127.0.0.1:3000';
+
+export const CAR_BRANDS = [
+  'Tesla',
+  'Ford',
+  'BMW',
+  'Audi',
+  'Mercedes',
+  'Toyota',
+  'Honda',
+  'Nissan',
+  'Volkswagen',
+  'Porsche',
+] as const;
+
+export const CAR_MODELS = [
+  'Model S',
+  'Mustang',
+  'X5',
+  'A4',
+  'C-Class',
+  'Camry',
+  'Civic',
+  'Altima',
+  'Golf',
+  '911',
+] as const;
